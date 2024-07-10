f0c3db7598db92283a2849a8b75ebb9a963af6bd
Nam Pham <pvnam95@hotmail.com>
2024-07-10 19:34:15 +0900

    chore: Update Home component to use react-navigation

--- components/customNavigator/Home.tsx
+++ components/customNavigator/Home.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Button } from "react-native";
 
 export default function Home() {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>Home</Text>
+      <Button
+        title="dispatch"
+        onPress={() => {
+          navigation.dispatch({
+            type: "FOO",
+            payload: { data: "BAR" },
+          });
+        }}
+      />
     </View>
   );
 }
